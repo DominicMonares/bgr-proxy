a65a0eea482883a23d60fa15e587f796007758c8
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-08 16:47:33 -0700

    comment

--- src/services/validateKey.js
+++ src/services/validateKey.js
@@ -4,6 +4,7 @@ const validateKey = () => {
   const id = process.env.CLIENT_ID;
   const secret = process.env.CLIENT_SECRET;
   // must send validation request to Twitch every hour
+  // start in same file as server listen
 }
 
 module.exports = {
