3b4d3a8062b7b22360f046be13f7f7200b3dd816
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-09 17:49:37 -0700

    implement getToken and validateToken

--- src/services/validateToken.js
+++ src/services/validateToken.js
@@ -4,20 +4,27 @@ const axios = require('axios');
 const getToken = () => {
   const id = process.env.CLIENT_ID;
   const secret = process.env.CLIENT_SECRET;
-  axios.post(`https://id.twitch.tv/oauth2/token?
-    client_id=${id}&
-    client_secret=${secret}&
-    grant_type=client_credentials
-  `, {})
-  .then(res => { console.log('RESULT ', res) })
-  .catch(err => { console.log('ERROR ', err) });
+  const grant = 'client_credentials';
+  const params = `client_id=${id}&client_secret=${secret}&grant_type=${grant}`;
+  const url = `https://id.twitch.tv/oauth2/token?${params}`;
+
+  return axios({ method: 'POST', url: url })
+    .then(res => res.data.access_token)
+    .catch(err => {
+      console.log('Failed to get token. Error: ', err);
+      // throw err;
+    });
 }
 
-const validateToken = () => {
-  const id = process.env.CLIENT_ID;
-  const secret = process.env.CLIENT_SECRET;
-  // must send validation request to Twitch every hour
-  // start in same file as server listen
+const validateToken = async () => {
+  const url = 'https://id.twitch.tv/oauth2/validate';
+  const token = await getToken();
+  axios.get(url, { 'Authorization': `OAuth ${token}` })
+    .then(res => { console.log('Hourly token validation successful!') })
+    .catch(err => {
+      console.log('Hourly token validation failed. Error: ', err);
+      // throw err;
+    })
 }
 
 module.exports = {
