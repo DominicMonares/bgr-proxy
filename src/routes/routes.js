488e0b4c2f974df8d79a3fb25aa7e43dcc463514
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-09 19:49:01 -0700

    cleanup

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const { getGameData } = require('../utils/getGameData.js');
+
 router.post('/games', async (req, res) => {
   res.send('GAME DATA HERE');
 })
