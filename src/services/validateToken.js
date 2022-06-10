e8faf2446ab9f8121775fbbb70ca188f59097319
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-09 19:22:46 -0700

    remove getToken

--- src/services/validateToken.js
+++ src/services/validateToken.js
@@ -1,21 +1,9 @@
 require('dotenv').config();
 const axios = require('axios');
 
-const getToken = () => {
-  const id = process.env.CLIENT_ID;
-  const secret = process.env.CLIENT_SECRET;
-  const grant = 'client_credentials';
-  const params = `client_id=${id}&client_secret=${secret}&grant_type=${grant}`;
-  const url = `https://id.twitch.tv/oauth2/token?${params}`;
-
-  return axios({ method: 'POST', url: url })
-    .then(res => res.data.access_token)
-    .catch(err => { throw `Failed to get token. Error: ${err}` });
-}
-
 const validateToken = async () => {
   const url = 'https://id.twitch.tv/oauth2/validate';
-  const token = await getToken();
+  const token = process.env.CLIENT_TOKEN;
   const config = {
     headers: {
       Authorization: `OAuth ${token}`
@@ -28,6 +16,5 @@ const validateToken = async () => {
 }
 
 module.exports = {
-  getToken: getToken,
   validateToken: validateToken
 }
