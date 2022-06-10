8d8d031a0af3a09b9514d1658477456d7eca0404
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-10 14:46:47 -0700

    add params

--- src/utils/getGameData.js
+++ src/utils/getGameData.js
@@ -3,18 +3,18 @@ const axios = require('axios');
 
 const { IGDB_URL } = require('../../constants.js');
 
-const getGameData = () => {
-  const headers = {
-    'Accept': 'application/json',
-    'Client-ID': process.env.CLIENT_ID,
-    'Authorization': `Bearer ${process.env.CLIENT_TOKEN}`
-  }
+const headers = {
+  'Accept': 'application/json',
+  'Client-ID': process.env.CLIENT_ID,
+  'Authorization': `Bearer ${process.env.CLIENT_TOKEN}`
+}
 
+const getGameData = () => {
   return axios({
     method: 'POST',
     url: `${IGDB_URL}/games`,
     headers: headers,
-    data: 'fields *;'
+    data: 'fields cover, genres, name, platforms, rating, url; sort rating asc;'
   })
     .then(res => res)
     .catch(err => { throw `Error fetching from IGDB: ${err}` });
