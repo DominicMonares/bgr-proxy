5c8c522a214914cae2932621bfff8f5727a07d41
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-10 20:02:12 -0700

    add optional page param

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 const { getGameData } = require('../services/getGameData.js');
 
-router.post('/games/:page', (req, res) => {
-  getGameData()
+router.post('/games/:page?', (req, res) => {
+  getGameData(req.params.page)
     .then(data => {
       res.send(data);
     })
     .catch(err => {
       res.status(500).send(err);
-    })
+    });
 })
 
 module.exports = router;
