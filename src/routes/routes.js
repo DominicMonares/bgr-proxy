35892cdcecf5fb8ebc9deb52b9aa69bcd45bf401
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-16 16:22:55 -0700

    add 1 sec delay

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,9 +5,12 @@ const { getGameData } = require('../services/getGameData.js');
 
 router.get('/games/:page?', (req, res) => {
   getGameData(req.params.page)
+    // 1 sec delay to avoid exceeding rate limit
     .then(data => {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.send(data);
+      setTimeout(() => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.send(data);
+      }, 1000)
     })
     .catch(err => {
       res.status(500).send(err);
